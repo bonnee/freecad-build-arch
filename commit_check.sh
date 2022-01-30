a035eac86e8e2e61114f8063ce42edd25d7a1b49
Bonnee <bonora.matteo@gmail.com>
2022-01-30 11:36:37 +0100

    Fix commit_check

--- commit_check.sh
+++ commit_check.sh
@@ -1,2 +1,2 @@
-exit $(curl -s https://api.github.com/repos/realthunder/FreeCAD/commits/LinkStage3 | jq -r "((now - (.commit.author.date | fromdateiso8601) )  / (60*60*24)  | trunc)")
+exit $(curl -s https://api.github.com/repos/realthunder/FreeCAD/commits/LinkDaily | jq -r "((now - (.commit.author.date | fromdateiso8601) )  / (60*60*24)  | trunc)")
 
