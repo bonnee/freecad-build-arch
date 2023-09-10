315d8fdf9f1ccc6f7e0e84a65f3659038f35ec18
Matteo Bonora <bonora.matteo@gmail.com>
2023-09-10 15:17:42 +0200

    Update commit_check.sh

--- utils/commit_check.sh
+++ utils/commit_check.sh
@@ -1,3 +1,3 @@
-exit $(curl -s https://api.github.com/repos/realthunder/FreeCAD/commits/LinkDaily | jq -r "((now - (.commit.author.date | fromdateiso8601) )  / (60*60*24)  | trunc)")
+exit $(curl -s https://api.github.com/repos/realthunder/FreeCAD/commits/LinkMerge | jq -r "((now - (.commit.author.date | fromdateiso8601) )  / (60*60*24)  | trunc)")
 
 # TODO: fetch AUR pkgver and compare to git version
